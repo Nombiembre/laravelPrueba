5a89cdd352d5973419d2a4ef9c5f0529a64e6a82
Nombiembre <emanuelt.marin9@gmail.com>
2025-10-16 14:23:24 -0500

    Added mini welcome/landing, finished projet

--- resources/views/components/common/header.blade.php
+++ resources/views/components/common/header.blade.php
@@ -1,8 +1,8 @@
 <header class="py-4 border-b border-gray-600 mb-6">
   <ul class="flex w-fit mx-auto gap-4">
-    <li><a class="px-2 py-1 hover:bg-blue-300 transition-all duration-300 rounded-md" href="/">Home</a></li>
-    <li><a class="px-2 py-1 hover:bg-blue-300 transition-all duration-300 rounded-md" href="/empresas">Empresas</a></li>
-    <li><a class="px-2 py-1 hover:bg-blue-300 transition-all duration-300 rounded-md" href="/participantes">Participantes</a></li>
-    <li><a class="px-2 py-1 hover:bg-blue-300 transition-all duration-300 rounded-md" href="/vacantes">Vacantes</a></li>
+    <li><a class="px-2 py-1 hover:bg-blue-700 hover:text-white transition-all duration-300 rounded-md" href="/">Home</a></li>
+    <li><a class="px-2 py-1 hover:bg-blue-700 hover:text-white transition-all duration-300 rounded-md" href="/empresas">Empresas</a></li>
+    <li><a class="px-2 py-1 hover:bg-blue-700 hover:text-white transition-all duration-300 rounded-md" href="/participantes">Participantes</a></li>
+    <li><a class="px-2 py-1 hover:bg-blue-700 hover:text-white transition-all duration-300 rounded-md" href="/vacantes">Vacantes</a></li>
   </ul>
 </header>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,14 @@
 <x-layout>
-    <h2>Home</h2>
+    <section class="max-w-6xl mx-auto px-4 py-20">
+        <div class="text-center">
+            <h1 class="text-5xl font-bold mb-6">Bienvenido a Empleo Sin Barreras</h1>
+            <p class="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
+                Conecta con oportunidades. Encuentra el talento ideal. Construye tu futuro.
+            </p>
+            <div class="flex gap-4 justify-center">
+                <a href="/vacantes" class="bg-blue-600 text-white px-8 py-3 rounded-lg duration-300 hover:bg-blue-700">Buscar Trabjos</a>
+                <a href="/vacantes/create" class="border border-blue-600 text-blue-600 px-8 duration-300 py-3 rounded-lg hover:bg-blue-50">Publicar un trabajo</a>
+            </div>
+        </div>
+    </section>
 </x-layout>
